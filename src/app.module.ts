5ad5f9e022bf6c1fc9ccfb748aab2aec0a65def7
lueberGandra <lueber_emp@hotmail.com>
2024-06-23 13:46:07 -0300

    feat(app): remove service, controller e test files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { SpotModule } from './spot/spot.module';
 
 @Module({
-  imports: [EventsModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [EventsModule, PrismaModule, SpotModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
